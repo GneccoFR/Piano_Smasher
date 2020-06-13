aa75c5ab29c60118848ead0207b6c492267f7f71
GneccoFR <GneccoFR@gmail.com>
2020-06-13 16:30:53 -0300

    Añadido proto UI  & multiple layer Piano

---
+++ Assets/MenuPiano.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void SimpleRow()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    
+        
+    }
+
+    public void DoubleRow()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+
+    }
+
+    public void TripleRow()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+    }
+
+    public void QuitGame ()
+    {
+      
+        Application.Quit();
+    }
+
+}
